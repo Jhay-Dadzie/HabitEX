3d7893d6c7070f53973f61125970f275f23ba41e
jhay-dadzie <josephdadzie0002@gmail.com>
2026-03-30 21:58:20 +0000

    Setting up project

---
+++ frontend/app/tenantScreens/index.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function index() {
+  return (
+    <View style={{display: "flex", flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Welcome to the tenants screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ frontend/contants/theme.ts
@@ -0,0 +1,53 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+import { Platform } from 'react-native';
+
+const tintColorLight = '#241A7F';
+const tintColorDark = '#1A247F';
+
+export const Colors = {
+  light: {
+    text: '#64748B',
+    background: '#fff',
+    tint: tintColorLight,
+    icon: '#94A3B8',
+    tabIconDefault: '#94A3B8',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#94A3B8',
+    background: '#0F172A',
+    tint: tintColorDark,
+    icon: '#64748B',
+    tabIconDefault: '#64748B',
+    tabIconSelected: tintColorDark,
+  },
+};
+
+export const Fonts = Platform.select({
+  ios: {
+    /** iOS `UIFontDescriptorSystemDesignDefault` */
+    sans: 'system-ui',
+    /** iOS `UIFontDescriptorSystemDesignSerif` */
+    serif: 'ui-serif',
+    /** iOS `UIFontDescriptorSystemDesignRounded` */
+    rounded: 'ui-rounded',
+    /** iOS `UIFontDescriptorSystemDesignMonospaced` */
+    mono: 'ui-monospace',
+  },
+  default: {
+    sans: 'inter',
+    serif: 'serif',
+    rounded: 'normal',
+    mono: 'monospace',
+  },
+  web: {
+    sans: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif",
+    serif: "Georgia, 'Times New Roman', serif",
+    rounded: "'SF Pro Rounded', 'Hiragino Maru Gothic ProN', Meiryo, 'MS PGothic', sans-serif",
+    mono: "SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+  },
+});
