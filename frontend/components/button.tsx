603e537e9f24a91aabe9dcd4bfd9b0b6eec7dcbf
jhay-dadzie <josephdadzie0002@gmail.com>
2026-04-02 18:08:10 +0000

    Global button component created

---
+++ frontend/components/button.tsx
@@ -0,0 +1,35 @@
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { Colors } from "@/constants/theme";
+import { Pressable, StyleSheet, TouchableOpacity } from "react-native";
+import { ThemedText } from "./themed-text";
+
+type buttonProps = {
+    buttonPlaceholder: string,
+    action?: () => void
+}
+export default function Button({ buttonPlaceholder, action }: buttonProps) {
+    const colorScheme = useColorScheme()
+    return (
+        <TouchableOpacity activeOpacity={0.8} onPress={action} style={[styles.button, {backgroundColor: Colors[colorScheme ?? 'light'].tint}]}>
+            <ThemedText type="defaultSemiBold" style={{color: '#fff'}}>{buttonPlaceholder}</ThemedText>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 20,
+        marginHorizontal: 20,
+        borderRadius: 26,
+        shadowColor: '#000',
+        shadowOpacity: 0.5,
+        shadowOffset: {width: 0, height: 2},
+        elevation: 5
+
+        
+    },
+    
+})
