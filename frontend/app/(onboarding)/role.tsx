187c596e4a151986d1b84d5133162494261a8493
jhay-dadzie <josephdadzie0002@gmail.com>
2026-04-02 23:06:19 +0000

    Added role selection screen

---
+++ frontend/app/(onboarding)/role.tsx
@@ -0,0 +1,135 @@
+import { Image, StyleSheet, Pressable, Text, View } from 'react-native'
+import { useRouter } from 'expo-router'
+import { ThemedText } from '@/components/themed-text'
+import { ThemedView } from '@/components/themed-view'
+import { Colors } from '@/constants/theme'
+import { useColorScheme } from '@/hooks/use-color-scheme'
+import { useState } from 'react'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import usePageStyles from '@/components/globalStyles/pageStyle'
+import Button from '@/components/button'
+import { MoveRight } from 'lucide-react-native'
+
+export default function Role() {
+  const router = useRouter()
+  const colorScheme = useColorScheme()
+  const colorThemeRenderer = usePageStyles()
+  const [selectedRole, setSelectedRole] = useState<'seeker' | 'landlord' | null>(null)
+  
+  const navigateToScreen = () => {
+    if (selectedRole === 'landlord') {
+      router.push('/(landlordScreens)')
+    } else {
+      router.push('/(tenantScreens)')
+    }
+  }
+
+  return (
+    <SafeAreaView style={[styles.container, {backgroundColor: Colors[colorScheme ?? 'light'].background}]}>
+      <ThemedView>
+        <ThemedText type='title'>Logo</ThemedText>
+      </ThemedView>
+
+      <ThemedView style={styles.headerContainer}>
+        <ThemedText type='title' style={[styles.text, {color: colorThemeRenderer.oppositeTextColor}]}>How would you like to use Habitex?</ThemedText>
+        <ThemedText type='description' style={styles.text}>Choose your journey to help us personalize your experience.</ThemedText>
+      </ThemedView>
+
+      <ThemedView style={[styles.cardContainer, {marginTop: 30}]}>
+        <Pressable onPress={() => setSelectedRole('seeker')}>
+
+          <ThemedView style={[styles.card,
+            {borderColor: selectedRole === 'seeker' ? Colors[colorScheme ?? 'light'].tint : colorThemeRenderer.borderColor}
+          ]}>
+            <Image source={require('@/assets/images/bedroom.jpg')} style={styles.image} />
+
+            <ThemedView style={styles.cardText}>
+              <ThemedText type='defaultSemiBold' style={{color: colorThemeRenderer.oppositeTextColor}}>House Seeker</ThemedText>
+              <ThemedText type='default'>Find your dream home from our curated verified listings.</ThemedText>
+            </ThemedView>
+
+          </ThemedView>
+        </Pressable>
+
+        <Pressable onPress={() => setSelectedRole('landlord')}>
+          <ThemedView style={[styles.card,
+            {borderColor: selectedRole === 'landlord' ? Colors[colorScheme ?? 'light'].tint : colorThemeRenderer.borderColor}
+          ]}>
+            <Image source={require('@/assets/images/lanlord.jpg')} style={styles.image} />
+
+            <ThemedView style={styles.cardText}>
+              <ThemedText type='defaultSemiBold' style={{color: colorThemeRenderer.oppositeTextColor}}>Landlord</ThemedText>
+              <ThemedText type='default'>List your property and connect with reliable tenants.</ThemedText>
+            </ThemedView>
+
+          </ThemedView>
+        </Pressable>
+        
+      </ThemedView>
+
+      {
+        selectedRole && (
+          <ThemedView style={{marginBottom: 50}}>
+            <Button buttonPlaceholder='Proceed' action={navigateToScreen}/>
+          </ThemedView>            
+        )
+      }
+    </SafeAreaView>
+    
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 5,
+  },
+  headerContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: 30,
+    padding: 20,
+    gap: 25
+  },
+  cardContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: 30,
+    padding: 20,
+    gap: 25
+  },
+  text: {
+    textAlign: 'center'
+  },
+  card: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 20,
+    borderRadius: 26,
+    borderWidth: 0.7,
+    padding: 15,
+    width: '100%',
+    height: 150,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 24,
+    elevation: 5
+
+  },
+  cardText: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    height: '100%',
+    marginTop: 5
+  },
+  image: {
+    overflow: 'hidden',
+    width: 100,
+    height: '100%',
+    borderRadius: 24
+  }
+})
