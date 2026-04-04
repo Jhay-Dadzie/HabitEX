62ba79a7280ae0e2cf1bd01007820918b6263178
jhay-dadzie <josephdadzie0002@gmail.com>
2026-04-04 04:27:41 +0000

    Renamed pageTheme

---
+++ frontend/components/globalStyles/pageThemeRender.tsx
@@ -0,0 +1,16 @@
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { Colors } from "@/constants/theme";
+
+export default function usePageThemeRender() {
+    const colorScheme = useColorScheme()
+    const light = colorScheme === 'light'
+
+    return (
+        {
+            oppositeTextColor:  light ? Colors.light.contrastColor : Colors.dark.contrastColor,
+            borderColor: light ? Colors.light.borderColor : Colors.dark.borderColor,
+            fontColor: light ? Colors.light.text : Colors.dark.text,
+            label: light ? '#334155' : Colors.dark.text
+        }
+    )
+}
