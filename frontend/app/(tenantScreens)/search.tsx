735db354222c22d8b263bf1ac73e8cb0bf127694
jhay-dadzie <josephdadzie0002@gmail.com>
2026-04-24 04:12:35 +0000

    import reusable search bar

--- frontend/app/(tenantScreens)/search.tsx
+++ frontend/app/(tenantScreens)/search.tsx
@@ -1,12 +1,14 @@
 import { StyleSheet } from 'react-native'
 import { ThemedText } from '@/components/themed-text'
 import { ThemedView } from '@/components/themed-view'
+import SearchBar from '@/components/searchBar'
 import React from 'react'
 
 export default function Search() {
   return (
     <ThemedView style={{flex: 1}}>
       <ThemedText>This is the search page</ThemedText>
+      <SearchBar autoFocus={true}/>
     </ThemedView>
   )
 }
